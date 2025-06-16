3bd5a4ce7b81dd554da294f21777a73e57bb358c
olha-dev-fullstack <olha.shevel.dev.@gmail.com>
2025-06-16 11:51:08 +0300

    add signin form

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import React from "react";
 
 const SignInPage = () => {
   return (
-    <div>SignIn</div>
-  )
-}
+    <div className="flex flex-col justify-center w-auto items-center min-h-screen">
+      <h2 className="text-2xl">Sign In</h2>
+      <form className="flex flex-col justify-center w-full max-w-sm gap-4">
+        <label htmlFor="email">Email</label>
+        <Input
+          name="email"
+          placeholder="Type your email..."
+          required
+          type="email"
+        />
+        <Button type="submit" className="max-w-fit">
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+};
 
-export default SignInPage
+export default SignInPage;
